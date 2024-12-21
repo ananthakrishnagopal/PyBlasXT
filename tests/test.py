df8422607c7e51f53445b74d992394859a780f65
ananthakrishna <50374726+ananthakrishnagopal@users.noreply.github.com>
2024-12-21 16:14:51 +0530

    updates

---
+++ tests/test.py
@@ -0,0 +1,7 @@
+from build.src import multiply
+
+import numpy as np
+
+a = np.random.rand(1000,5).astype(np.float32)
+b = np.random.rand(5,1000).astype(np.float32)
+print(np.linalg.norm(multiply.multiply(a,b,np.array([0])).T-a@b))
