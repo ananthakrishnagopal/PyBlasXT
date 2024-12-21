40647e553ccf5c7cf00d47a8ff74c77026de5daa
ananthakrishna <50374726+ananthakrishnagopal@users.noreply.github.com>
2024-12-21 16:29:07 +0530

    first working version

--- build/test.py
+++
@@ -1,7 +0,0 @@
-from src import multiply
-
-import numpy as np
-
-a = np.random.rand(1000,5).astype(np.float32)
-b = np.random.rand(5,1000).astype(np.float32)
-print(np.linalg.norm(multiply.multiply(a,b,np.array([0]))-a@b))
