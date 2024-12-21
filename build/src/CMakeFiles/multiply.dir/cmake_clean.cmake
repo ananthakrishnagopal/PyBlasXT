a7fdaca9076cf3e3f8e012d532b92dbd9c2eb8ed
ananthakrishna <50374726+ananthakrishnagopal@users.noreply.github.com>
2024-12-21 15:52:09 +0530

    updates

---
+++ build/src/CMakeFiles/multiply.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multiply.dir/sgemm.cu.o"
+  "CMakeFiles/multiply.dir/sgemm.cu.o.d"
+  "multiply.cpython-310-x86_64-linux-gnu.so"
+  "multiply.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/multiply.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
